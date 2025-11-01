e50d63d1bdf7d1674bfa6ca1cf850702d7ff096e
rama <jihanlobs@gmail.com>
2025-11-02 03:29:32 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/myreservation", "/admin", "/checkout"];
 
+// expose api route to midtrans
+export const config = {
+	matcher: ["/api/payment/:id", "/api/payment/notification"]
+}
+
 export async function middleware(req: NextRequest) {
 	const token = req.cookies.get("authjs.session-token") || req.cookies.get("__Secure-authjs.session-token");
 	const isLoggedIn = !!token;
@@ -19,7 +24,3 @@ export async function middleware(req: NextRequest) {
 	// For admin routes, let the page component handle role verification
 	return NextResponse.next();
 }
-
-export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
-};
