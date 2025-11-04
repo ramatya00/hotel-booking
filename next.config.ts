935add79a415c24dc1211f8f7e03e032d5424332
rama <jihanlobs@gmail.com>
2025-11-04 18:36:49 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async headers() {
+    return [
+      {
+        source: "/api/payment/notification/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST" },
+          {
+            key: "Access-Control-Allow-Headers", value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+          },
+        ]
+      }
+    ]
+  },
   images: {
-    domains: ["lh3.googleusercontent.com", "jv07rtjkkzkqffzh.public.blob.vercel-storage.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "jv07rtjkkzkqffzh.public.blob.vercel-storage.com",
+      }
+    ]
   },
 };
 
